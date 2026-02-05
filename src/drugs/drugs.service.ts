cf29018a2a69e334fb794b29b060a6055303dd35
mhardiansyah <mhardiansyah5766@gmail.com>
2026-02-05 16:44:53 +0700

    chore: add gitignore and fix backend structure

--- src/drugs/drugs.service.ts
+++ src/drugs/drugs.service.ts
@@ -1,7 +1,7 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateDrugDto } from './dto/create-drug.dto';
 import { UpdateDrugDto } from './dto/update-drug.dto';
-import { PrismaService } from 'src/prisma/prisma.service';
+import { PrismaService } from 'prisma/prisma.service';
 
 @Injectable()
 export class DrugsService {
