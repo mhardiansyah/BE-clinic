ceec10c2de70f30f6f96df79eb3c589a0d0ff1f7
mhardiansyah <mhardiansyah5766@gmail.com>
2026-02-05 12:07:02 +0700

    chore: add gitignore and fix backend structure

--- src/drugs/drugs.controller.ts
+++ src/drugs/drugs.controller.ts
@@ -12,7 +12,6 @@ import { DrugsService } from './drugs.service';
 import { CreateDrugDto } from './dto/create-drug.dto';
 import { UpdateDrugDto } from './dto/update-drug.dto';
 import { AuthGuard } from '@nestjs/passport';
-@AuthGuard()
 @Controller('drugs')
 export class DrugsController {
   constructor(private readonly drugsService: DrugsService) {}
